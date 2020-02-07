592af6433cf0eb5f4609964744e29be193cb5dca
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-02-07 21:14:05 +0100

    feat: remove fluff

---
+++ src/app/zippy.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, NgModule, ɵmarkDirty } from '@angular/core';
+
+import { ButtonDirective } from './button.directive';
+
+  
+  
+  @Component({
+    selector: 'app-zippy',
+    template: `
+      <button appButton (appClick)="onToggle()">
+        {{label}}
+      </button>
+  
+      <div [hidden]="!isExpanded">
+        <ng-content></ng-content>
+      </div>
+    `,
+  })
+  export class ZippyComponent {
+    @Input()
+    label = 'Toggle';
+  
+    isExpanded = false;
+  
+    onToggle() {
+      this.isExpanded = !this.isExpanded;
+      ɵmarkDirty(this);
+    }
+  }
+  
+  @NgModule({
+    declarations: [
+      ZippyComponent,
+      ButtonDirective,
+    ],
+  })
+  class ZippyRenderModule {}
